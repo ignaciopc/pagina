008495895c875befde75eec3f9e8b7b3d15f244d
ignaciopc <ignaciopalomarchacon.com>
2025-02-07 11:01:55 +0100

    first commit

---
+++ scr/header.js
@@ -0,0 +1,6 @@
+    fetch('./componentes/header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-container').innerHTML = data;
+        })
+        .catch(error => console.error('Error al cargar el header:', error));
